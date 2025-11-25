a7ba05dfd9e1df4381001636cf33ee6b49059516
PedroSanchez69 <seb.millahual@duocuc.cl>
2025-11-24 23:48:16 -0300

    si

---
+++ src/main/java/com/example/CalgachaIndustries/CalgachaIndustriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalgachaIndustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalgachaIndustriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalgachaIndustriesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/CalgachaIndustries/repository/GallinaRepository.java
@@ -0,0 +1,9 @@
+package com.example.CalgachaIndustries.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.CalgachaIndustries.model.Gallina;
+
+public interface GallinaRepository extends JpaRepository<Gallina, Long> {
+
+}
